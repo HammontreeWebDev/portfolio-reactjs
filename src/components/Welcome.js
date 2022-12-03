b8edcbcf2b54b625770ee58f46b0b724247ec57d
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-03 13:11:57 -0500

    landing page looking good, need to add animations

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../assets/css/welcome.css';
+import transNameLogo from '../assets/img/transparent-name-logo.png';
 
 function Welcome() {
 
     return (
-        <main>
-            <h2>
-                Welcome.
-            </h2>
+        <main className='homepage-main'>
+            <img className='homepage-img' src={transNameLogo} alt='image/logo of laptop with Kaileb Hammontree Full-Stack Web Development printed underneath'/>
         </main>
     );
 
