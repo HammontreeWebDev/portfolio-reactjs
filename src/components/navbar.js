98f8bf6767f8a004006dbcf9f1a08579eedd8acc
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-03 00:41:56 -0500

    navbar styling almost complete

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,11 +5,11 @@ function Navbar(props) {
   return (
       <nav className='navbar nav-bg'>
         <div className='container-fluid'>
-          <h1>Kaileb Hammontree</h1>
-          <a className='navbar-brand' href="#">{props.about}</a>
-          <a className='navbar-brand' href="#">{props.portfolio}</a>
-          <a className='navbar-brand' href="#">{props.contact}</a>
-          <a className='navbar-brand' href="#">{props.resume}</a>
+          <h1 className='name-font'>Kaileb Hammontree</h1>
+          <a className='nav-font' href="#">{props.about}</a>
+          <a className='nav-font' href="#">{props.portfolio}</a>
+          <a className='nav-font' href="#">{props.contact}</a>
+          <a className='nav-font' href="#">{props.resume}</a>
         </div>
       </nav>
   );
