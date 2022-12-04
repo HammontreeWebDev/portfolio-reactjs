97aa27afe8c6655c0506c8f089bf7ab64ce8e229
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-03 21:12:50 -0500

    hvr-buzz effect added to nav bar a tags

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,10 +10,10 @@ export function NavBar(props) {
       <nav className='navbar main-nav-bg'>
         <div className='container-fluid'>
           <img className='nav-img' src={navNameLogo} alt='image/logo of laptop with Kaileb Hammontree Full-Stack Web Development printed underneath' />
-          <a className='nav-font hvr-grow' href="#">{props.about}</a>
-          <a className='nav-font' href="#">{props.portfolio}</a>
-          <a className='nav-font' href="#">{props.contact}</a>
-          <a className='nav-font' href="#">{props.resume}</a>
+          <a className='nav-font hvr-buzz' href="#">{props.about}</a>
+          <a className='nav-font hvr-buzz' href="#">{props.portfolio}</a>
+          <a className='nav-font hvr-buzz' href="#">{props.contact}</a>
+          <a className='nav-font hvr-buzz' href="#">{props.resume}</a>
         </div>
       </nav>
     </BounceInDownDiv>
