89dee05a8b73eecdcefa1842877b8a73b2bba65c
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-04 01:08:11 -0500

    css styling close to being done for about me card.. may add a border... most content is there, will probably revisit to add actual about me content and just leave placeholder content there for now

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import '../assets/css/aboutme.css';
 
 function AboutMe() {
+
     return (
-        <main>
-            <section class="card custom-card">
-                <header class="card-header">
-                    Featured
-                </header>
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <main className='about-me-body'>
+            <section className=' custom-card'>
+                <h2 className='custom-card-header'>
+                    About Me
+                </h2>
+                <div className='custom-card-body'>
+                    <h5 className='card-title'>Kaileb Hammontree | Web Developer</h5>
+                    <p className='card-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis lectus nulla at volutpat diam ut venenatis. Volutpat ac tincidunt vitae semper quis lectus. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Scelerisque purus semper eget duis at tellus. Luctus accumsan tortor posuere ac ut. Nunc sed augue lacus viverra vitae. Parturient montes nascetur ridiculus mus mauris. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Dolor magna eget est lorem ipsum dolor sit.
+                        <br />
+                        <br />
+                        Amet justo donec enim diam vulputate ut. Adipiscing bibendum est ultricies integer quis. Enim neque volutpat ac tincidunt. Sed pulvinar proin gravida hendrerit lectus. At varius vel pharetra vel turpis. At consectetur lorem donec massa sapien faucibus et. Quam vulputate dignissim suspendisse in est. Morbi quis commodo odio aenean sed adipiscing diam. Laoreet sit amet cursus sit amet dictum sit amet. Aenean et tortor at risus viverra adipiscing at in tellus. Scelerisque purus semper eget duis at. Et malesuada fames ac turpis egestas. Laoreet id donec ultrices tincidunt arcu. Ornare massa eget egestas purus viverra accumsan. Sodales neque sodales ut etiam sit. Eget nunc scelerisque viverra mauris in aliquam sem fringilla. Viverra justo nec ultrices dui sapien eget mi proin. Scelerisque purus semper eget duis at. Consequat id porta nibh venenatis cras sed felis eget.
+
+                    </p>
                 </div>
             </section>
         </main>
