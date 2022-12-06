34fd0a9b1bbbe2f7523a1c430264cf6a51304a31
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-05 23:57:19 -0500

    content for resume added, media query added, may change font sizes

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,66 +1,88 @@
 import React from 'react';
 import './resume.css';
 
-// we copied the about me card here, so it is inheriting the custom card classes from that css file
+// we copied the about me card here, so it is inheriting some of the custom card classes from that css file
 function Resume() {
     return (
         <main className='about-me-body'>
             <section className=' custom-card'>
-                <h2 className='custom-card-header'>
-                    Resume
-                </h2>
-                <div className='custom-card-body'>
+                <div className='resume-card-header'>
+                    <h2> Resume </h2>
+                    <button>Download My Official Resume Here</button>
+                </div>
+                <div className='custom-card-body resume-body'>
                     <h5 className='skill-title'>Front-End Skills:</h5>
                     <ul className='skill-container'>
                         <div className='skill-item'>
-                        <li>HTML5</li>
-                        <li>JavaScript</li>
-                        <li>CSS (Custom, Bootstrap, Bulma)</li>
-                        <li>Node.js</li>
+                            <li>HTML5</li>
+                            <li>JavaScript</li>
+                            <li>CSS (Flexbox / Custom)</li>
+                            <li>CSS Framework (Bulma, Bootstrap)</li>
                         </div>
 
                         <div className='skill-item'>
-                        <li>HTML5</li>
-                        <li>JavaScript</li>
-                        <li>CSS (Custom, Bootstrap, Bulma)</li>
-                        <li>Node.js</li>
+                            <li>Local Storage/Session Storage</li>
+                            <li>jQuery</li>
+                            <li>API (JSON, AJAX, REST, Web, 3rd party)</li>
+                            <li>React.js</li>
+                            <li>Handlebars</li>
                         </div>
                     </ul >
 
                     <h5 className='skill-title'>Back-End Skills:</h5>
                     <ul className='skill-container'>
                         <div className='skill-item'>
-                        <li>HTML5</li>
-                        <li>JavaScript</li>
-                        <li>CSS (Custom, Bootstrap, Bulma)</li>
-                        <li>Node.js</li>
+                            <li>SQL / MySQL (DBeaver)</li>
+                            <li>Node.js</li>
+                            <li>Express.js (Insomnia)</li>
+                            <li>MongoDB (Mongoose)</li>
                         </div>
 
                         <div className='skill-item'>
-                        <li>HTML5</li>
-                        <li>JavaScript</li>
-                        <li>CSS (Custom, Bootstrap, Bulma)</li>
-                        <li>Node.js</li>
+                            <li>Server-Side API Development</li>
+                            <li>MERN Stack (in progress)</li>
+                            <li>C# (in progress)</li>
+                            <li>Python (advanced concepts in progress)</li>
+                            <li>Java (in progress)</li>
                         </div>
                     </ul >
 
-                    <h5 className='skill-title'>Other Skills & Technology:</h5>
+                    <h5 className='skill-title'>General Skills & Technology:</h5>
                     <ul className='skill-container'>
                         <div className='skill-item'>
-                        <li>HTML5</li>
-                        <li>JavaScript</li>
-                        <li>CSS (Custom, Bootstrap, Bulma)</li>
-                        <li>Node.js</li>
+                            <li>Babeljs</li>
+                            <li>Git (version control)</li>
+                            <li>GitHub / GitHub Pages</li>
+                            <li>Heroku</li>
                         </div>
 
                         <div className='skill-item'>
-                        <li>HTML5</li>
-                        <li>JavaScript</li>
-                        <li>CSS (Custom, Bootstrap, Bulma)</li>
-                        <li>Node.js</li>
+                            <li>Progressive Web Applications</li>
+                            <li>Algorithms</li>
+                            <li>Agile Methodology</li>
+                            <li></li>
                         </div>
                     </ul >
-                    <button>Download My Official Resume Here</button>
+
+                    <h5 className='skill-title'>Cyber Security Knowledge:</h5>
+                    <ul className='skill-container'>
+                    <div className='skill-item'>
+                            <li>Linux Security</li>
+                            <li>Microsoft Security</li>
+                            <li>Network Security</li>
+                            <li>Cloud Security</li>
+                            <li>Ethical Hacking</li>
+                        </div>
+
+                        <div className='skill-item'>
+                            <li>Computer Networking</li>
+                            <li>Cyber Infrastructure & Technology</li>
+                            <li>Digital Forensics And Incident Response</li>
+                            <li>Game Theory</li>
+                            <li>Amazon Web Services</li>
+                        </div>
+                    </ul >
+
                 </div>
             </section>
         </main>
