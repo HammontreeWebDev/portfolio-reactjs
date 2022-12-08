5cbc00d288ee675c5b2eb67fd63f55c4e8a310dd
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-07 20:30:51 -0500

    footer is functional

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import './footer.css';
 import { Icon, enableCache } from '@iconify/react';
+import Swal from 'sweetalert2';
 
 // allow iconify icons to be stored locally for quicker rendering
 enableCache('local');
 
 export default function Footer() {
+
+    // alert until linkedin is active again
+    const linkedInAlert = () => {
+        Swal.fire({
+            icon: 'info',
+            title: 'LinkedIn Unavailable',
+            text: 'Sorry, that this is not available right now. Please check back soon.'
+        })
+    }
+
     return (
         <footer>
-        <Icon className="icon" icon="fa:github-square" />
-        <Icon className="icon" icon="brandico:linkedin-rect" />
-        <Icon className="icon" icon="cib:stackoverflow" />
+        <a className='hvr-buzz icon' href="https://github.com/HammontreeWebDev" ><Icon icon="fa:github-square" /></a>
+        <a className='hvr-buzz icon' onClick={linkedInAlert} ><Icon icon="brandico:linkedin-rect" /></a>
+        <a className='hvr-buzz icon' href="https://stackoverflow.com/users/20187731/hammontreewebdev"><Icon icon="cib:stackoverflow" /></a>
         </footer>
         
     )
