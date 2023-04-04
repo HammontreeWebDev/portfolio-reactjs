edf0d14d4b6bc212adae4e79b50abb380f6b94c8
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-04-04 15:06:52 -0400

    updated the about me section to reflect current information

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -23,15 +23,17 @@ function AboutMe() {
                         My journey began in 2021 when I graduated from The University Of Central Florida's Cyber Defense Professional Certificate Program. We spent 10 months learning several things from Computer Networking / Network Security all the way to Ethical Hacking and Game Theory. While I did enjoy the education I had recieved, I realized that cyber security was not what I actually wanted to do. The course that we took in Python opened up my eyes to all the cool things that programming had to offer and so I began building silly text based games that only worked in the CLI and unashamedly began showing all my friends and family.
                         <br />
                         <br />
-                        Thankfully, I've come a long way from CLI text-based games and can now build Full-Stack Applications. In 2022, I decided to join the Full-Stack Web Development Bootcamp with UCF, so that I could gain the knowledge and skills necessary to fuel my creative nature. At this moment, we are in the final weeks of the bootcamp and I am expected to graduate Mid-December 2022. Every group project that I have been a part of has recieved high praise from my peers and leaders. There is no doubt that my work ethic, practical experience, and knowledge will allow me to tackle any project that comes my way, including yours.
+                        Thankfully, I've come a long way from CLI text-based games and can now build Full-Stack Applications. In 2022, I decided to join the Full-Stack Web Development Bootcamp with UCF, so that I could gain the knowledge and skills necessary to fuel my creative nature. I graduated with an A+ in December 2022. Every group project that I have been a part of has recieved high praise from my peers and leaders. There is no doubt that my work ethic, practical experience, and knowledge will allow me to tackle any project that comes my way, including yours.
                         <br />
                         <br />
-                        Feel free to navigate to the portfolio tab and check out various projects that I have completed. Hover over the preview of the project for more details on the technology that was used to build it and click on the preview to be redirected to the application itself.
+                        Feel free to navigate to the portfolio tab and check out various projects that I have completed. Hover over the preview of the project for more details on the technology that was used to build it and click on the preview icon to be redirected to the application itself.
                         <br />
                         <br />
                         Again, thank you for being here. I am looking forward to hearing from you!
                         <br />
                         <br />
+
+                        FYI: This website was also built by me, using reactJS!
                         
 
                     </p>
