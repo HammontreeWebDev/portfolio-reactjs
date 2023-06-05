41b66604c5b00963a44a95c8063cb666e9f36dc3
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-04 20:51:20 -0400

    changed about me title

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -14,7 +14,7 @@ function AboutMe() {
                 <div className='custom-card-body'>
                     <div className='name-container'>
                         <img className='avatar' src={avatar} alt='avatar of Kaileb Hammontree' />
-                        <h5 className='card-title'><span className='card-name'>Kaileb Hammontree</span> | <span className='text-pink'>Web Developer</span></h5>
+                        <h5 className='card-title'><span className='card-name'>Kaileb Hammontree</span> | <span className='text-pink'>Full-Stack Developer</span></h5>
                     </div>
                     <p className='card-text'>
                         First of all, I want to thank you for taking the time out of your day to visit my website. We live in a society where most people turn to technology to make their lives easier. Sitting behind every application, website, gadget, or tool, there is someone who spent hours, days, months, or even years planning, developing, and debugging the very pieces of technology that we have all come to love and enjoy. My goal as a Full-Stack Web Developer is to do that for you.
