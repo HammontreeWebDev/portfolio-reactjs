4e8a40c21fd114a57e0b22b6811473e257ab0a56
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-04 22:40:01 -0400

    added new avatar

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './aboutme.css';
 import { FlipInXMain } from '../../utils/Animations'
-import avatar from '../../assets/img/avatar.webp'
+import avatar from '../../assets/img/avatar.jpeg'
 
 function AboutMe() {
 
