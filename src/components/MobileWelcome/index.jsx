b9499d6a936d064324e5d9a801f69e70b60b0c48
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-05 11:24:30 -0400

    use js to play video on mobile devices

--- src/components/MobileWelcome/index.jsx
+++ src/components/MobileWelcome/index.jsx
@@ -3,9 +3,17 @@ import './assets/css/mobile-welcome.css';
 import mobileVideo from './assets/vid/mobile-intro.mp4';
 
 const MobileWelcome = () => {
+    const mobileIntrolEl = document.querySelector('#mobile-intro');
+
+    async function playVideo() {
+        await mobileIntrolEl.play();
+    }
+
+    playVideo();
+
     return (
         <section className="mobile-container">
-            <video className="mobile-video" preload="auto" muted autoPlay loop src={mobileVideo} />
+            <video id="mobile-intro" className="mobile-video" preload="auto" autoPlay muted loop src={mobileVideo} />
         </section>
     )
 }
