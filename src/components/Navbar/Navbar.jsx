4e2356ebcad620e9f8188c6015f5e456982fac15
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-05 14:37:37 -0400

    changed out header for webp format

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './navbar.css';
 import { BounceInDownHeader } from '../../utils/Animations';
-import navNameLogo from '../../assets/img/trans-web-header.png';
+import navNameLogo from '../../assets/img/trans-web-header.webp';
 import 'hover.css';
 
 export function NavBar(props) {
@@ -11,11 +11,11 @@ export function NavBar(props) {
     <BounceInDownHeader className='header-bg'>
       <img className='nav-img' src={navNameLogo} alt='Avatar of Kaileb with his name and text that says Full-Stack Web Development'/>
       <nav className='container-fluid custom-nav'>
-        <a className={'nav-font hvr-buzz'} onClick={props.showAbout} >{props.about}</a>
+        <button className={'nav-font hvr-buzz'} onClick={props.showAbout} >{props.about}</button>
         <a className={'nav-font hvr-buzz'} href='#copyright'>Socials</a>
-        <a className='nav-font hvr-buzz' onClick={props.showPortfolio} >{props.portfolio}</a>
-        <a className='nav-font hvr-buzz' onClick={props.showContact} >{props.contact}</a>
-        <a className='nav-font hvr-buzz' onClick={props.showResume} >{props.resume}</a>
+        <button className='nav-font hvr-buzz' onClick={props.showPortfolio} >{props.portfolio}</button>
+        <button className='nav-font hvr-buzz' onClick={props.showContact} >{props.contact}</button>
+        <button className='nav-font hvr-buzz' onClick={props.showResume} >{props.resume}</button>
       </nav>
     </BounceInDownHeader>
   );
