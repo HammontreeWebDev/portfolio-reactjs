0f7849466a9ae3c3d9c301eafc7b3484ce4cd651
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-05 12:03:04 -0400

    removed autoplay and added a thumbnail

--- src/components/MobileWelcome/index.jsx
+++ src/components/MobileWelcome/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './assets/css/mobile-welcome.css';
 import mobileVideo from './assets/vid/mobile-intro.mp4';
+import mobileThumb from './assets/img/thumbnail.png';
 
 const MobileWelcome = () => {
     // const mobileIntrolEl = document.querySelector('#mobile-intro');
@@ -13,7 +14,7 @@ const MobileWelcome = () => {
 
     return (
         <section className="mobile-container">
-            <video id="mobile-intro" className="mobile-video" preload="auto" autoPlay={true} muted={true} loop={true} controls={true} src={mobileVideo} />
+            <video id="mobile-intro" className="mobile-video" preload="auto" controls={true} src={mobileVideo} poster={mobileThumb} />
         </section>
     )
 }
