f6f9c6def1f183db66eee1dab5bb7908ab300616
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-11 00:09:43 -0400

    Updates

---
+++ precache-manifest.4986dc6dd99f76448e0f9bc851641ca2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c87315f1574206816a0",
+    "url": "/static/css/main.b29732bb.chunk.css"
+  },
+  {
+    "revision": "6c87315f1574206816a0",
+    "url": "/static/js/main.6c87315f.chunk.js"
+  },
+  {
+    "revision": "25f883c26fa2b192f694",
+    "url": "/static/css/1.7d02fc7f.chunk.css"
+  },
+  {
+    "revision": "25f883c26fa2b192f694",
+    "url": "/static/js/1.25f883c2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a6512e9095d83c22d1083b2e5469add",
+    "url": "/static/media/trans-web-header.3a6512e9.webp"
+  },
+  {
+    "revision": "9faef537807307a7b3f2de39b51c1d57",
+    "url": "/static/media/avatar.9faef537.webp"
+  },
+  {
+    "revision": "3fce44f8ad6595d08630038a305f3424",
+    "url": "/static/media/Technical-Resume.3fce44f8.pdf"
+  },
+  {
+    "revision": "2fbf128f30c4dc81c6b805eec477313f",
+    "url": "/static/media/final-web-intro.2fbf128f.mp4"
+  },
+  {
+    "revision": "50274e73523c565d88591fb6b0884ee2",
+    "url": "/static/media/mobile-intro.50274e73.mp4"
+  },
+  {
+    "revision": "373e6fd32f02bf0f4030ab4b98462a54",
+    "url": "/static/media/mtgpp.373e6fd3.webp"
+  },
+  {
+    "revision": "cd27b443b1882f2e14e4388607e06f86",
+    "url": "/static/media/pokewire.cd27b443.webp"
+  },
+  {
+    "revision": "f6ae2738be8726a77164ee68abf87440",
+    "url": "/static/media/outta-touch.f6ae2738.webp"
+  },
+  {
+    "revision": "016709686c23bfd1bee15f6f57bef5d1",
+    "url": "/static/media/brofit.01670968.webp"
+  },
+  {
+    "revision": "5033870229f09850ac9589e694f5655a",
+    "url": "/static/media/background.50338702.webp"
+  },
+  {
+    "revision": "f0d343760d7f4b2a508408a42e0b4134",
+    "url": "/index.html"
+  }
+];
